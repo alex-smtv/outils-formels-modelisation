f7ba252fbcc85c6c5a37609c3cc78ab832317066
Alexandre Simoes Tavares <tvrs.alexandre@yahoo.com>
2017-10-22 17:03:38 +0200

    Add graph generation (question 3) and demonstration (question 4)

--- tp-02/Smokers/Sources/Smokers/main.swift
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -25,4 +25,74 @@ let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2
 // Create the marking graph (if possible).
 if let markingGraph = model.markingGraph(from: initialMarking) {
     // Write here the code necessary to answer questions of Exercise 4.
+
+    var nodesToVisit     : [MarkingGraph] = [markingGraph]
+    var allMarkingsFound : [PTMarking]    = []
+
+    // We'll navigate through the graph to catch all unique markings
+    while !nodesToVisit.isEmpty {
+
+        let node = nodesToVisit.removeFirst()
+        allMarkingsFound.append(node.marking)
+
+        // Check for each successor node
+        for nextNode in node.successors.values {
+
+            // Do something only if the successor marking isn't one we already found AND if the successor marking isn't the one from one node planned to visit
+            if !allMarkingsFound.contains(where: { $0 == nextNode.marking }) && nodesToVisit.first(where: { $0.marking == nextNode.marking }) == nil {
+                nodesToVisit.append(nextNode)
+            }
+
+        }
+
+    }
+
+    print("Question 4.1) We found \(allMarkingsFound.count) unique markings. In consequence there is \(allMarkingsFound.count) different states in our network.")
+    print()
+
+    var existTwoSmokerSmoking    = false
+    var existTwoOfSameIngredient = false
+
+    // Analyse each marking for answering the questions
+    for marking in allMarkingsFound {
+
+        // Get tokens from smoking smokers
+        let s1 = marking.first(where: { $0.key.name == "s1" })!.value
+        let s2 = marking.first(where: { $0.key.name == "s2" })!.value
+        let s3 = marking.first(where: { $0.key.name == "s3" })!.value
+
+        // Get tokens from ingredients
+        let p = marking.first(where: { $0.key.name == "p" })!.value
+        let t = marking.first(where: { $0.key.name == "t" })!.value
+        let m = marking.first(where: { $0.key.name == "m" })!.value
+
+        // If at least two smoking smokers is found
+        if !existTwoSmokerSmoking && ( (s1 == 1 && s2 == 1) || (s1 == 1 && s3 == 1) || (s2 == 1 && s3 == 1) ) {
+            existTwoSmokerSmoking = true
+            print("Question 4.2) At least one occurrence of two different smokers smoking at the same time has been found. So yes, it's possible that two different smokers are smoking at the same time.")
+            print("  - This is the matching marking answering the question: \(marking)")
+            print()
+        }
+
+        // If at least one ingredient with a quantity of 2 is found
+        if !existTwoOfSameIngredient && (p == 2 || t == 2 || m == 2) {
+            existTwoOfSameIngredient = true
+            print("Question 4.3) The answer to the question is no: it's not possible to have two quantity of the same ingredient on the table. Unfortunately the application found a scenario of this happening: an unwanted error is present.")
+            print("  - This is the (false) matching marking to the question: \(marking)")
+            print()
+        }
+
+    }
+
+    if !existTwoSmokerSmoking {
+        print("Question 4.2) The answer to the question is yes: two different smokers can be smoking at the same time. Unfortunately the application didn't find one such scenario: an unwanted error is present.")
+        print()
+    }
+
+    if !existTwoOfSameIngredient {
+        print("Question 4.3) From all the ingredients none is found twice on the table. So no, it's not possible to have two quantity of the same ingredient on the table.")
+        print()
+    }
+
+    print("End of the demonstration.")
 }
--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -16,7 +16,48 @@ public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
         // Write here the implementation of the marking graph generation.
-        return nil
+
+        let firstNode = MarkingGraph(marking: marking)
+
+        var nodesToVisit : [MarkingGraph] = [firstNode]
+        var nodesVisited : [MarkingGraph] = []
+
+        while !nodesToVisit.isEmpty {
+
+            let node = nodesToVisit.removeFirst()
+            nodesVisited.append(node)
+
+            // Check for each transition which one is fireable from the current node marking
+            for transition in transitions {
+
+                if let newMarking = transition.fire(from: node.marking) {
+                    
+                    // If the transition is firable, we're going to determine which node will be the successor
+                    var nextNode: MarkingGraph? = nil
+
+                    // Check if the new marking is already one of the node already visited or to be visited and if it's the case, get that node
+                    if let nodeAlreadyVisited = nodesVisited.first(where: { $0.marking == newMarking }) {
+                        nextNode = nodeAlreadyVisited
+                    } 
+                    else if let nodeToBeVisited = nodesToVisit.first(where: { $0.marking == newMarking }) {
+                        nextNode = nodeToBeVisited
+                    }
+                    
+                    // If no existing node corresponding to the new marking is found, create the node
+                    if (nextNode == nil) {
+                        nextNode = MarkingGraph(marking: newMarking)
+                        nodesToVisit.append(nextNode!)
+                    }
+                    
+                    // Add the next node to the list of successors 
+                    node.successors[transition] = nextNode
+                }
+
+            }
+
+        }
+
+        return firstNode
     }
 
 }
